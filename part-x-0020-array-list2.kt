b3300ef9b8cfca1c32c36cc4ce2ae47824d830b5
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-24 10:41:18 +0300

    Create part-x-0020-array-list2.kt

---
+++ part-x-0020-array-list2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+    val names = arrayListOf("ayhan", "hakan", "bengü")
+    
+    names.add(1, "bilge")
+    
+    println(names.toString()) // [ayhan, bilge, hakan, bengü]
+ }
+
