fcc2eb7f66941e3156ada6f6e194ece4cd2fd13e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-24 09:48:18 +0300

    Create part-x-0031-random1.kt

---
+++ part-x-0031-random1.kt
@@ -0,0 +1,24 @@
+import kotlin.random.Random
+
+fun main() {
+
+	val ranN = Random.nextInt(0, 15)
+    
+    val ranB = Random.nextBoolean()
+    
+    val ranD = Random.nextDouble(0.7, 2.54)
+    
+    println(ranN)
+    
+    println(ranB)
+    
+    println(ranD)
+    
+    
+ }
+
+// no constant values! they will change 
+
+// 6
+// true
+// 0.7039344789257473
