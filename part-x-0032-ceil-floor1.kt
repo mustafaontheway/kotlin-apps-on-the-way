c12836a79e35c9194fdf75b0d599c0c90c7cc992
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-24 09:59:08 +0300

    Create part-x-0032-ceil-floor1.kt

---
+++ part-x-0032-ceil-floor1.kt
@@ -0,0 +1,13 @@
+import kotlin.math.*
+
+fun main() {
+    
+	val num = 4.66
+    
+    val numC = ceil(num)
+    
+    val numF = floor(num)
+    
+    println("$num - $numC - $numF") // 4.66 - 5.0 - 4.0
+ }
+
